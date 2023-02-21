98404a6f4d5f4a0ba50c32c60e75f88ff6d19e37
Andy Pham <andyphamhp9x@gmail.com>
2023-02-21 16:04:30 +0700

    Initial Commit

---
+++ GoogleSheetNotification/GoogleSheetNotificationApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoogleSheetNotificationApp.swift
+//  GoogleSheetNotification
+//
+//  Created by Andy Pham on 21/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct GoogleSheetNotificationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
